4209136b4b154cba754058860030d27856ec48d6
Alien <ctwjun90@gmail.com>
2017-03-05 01:01:26 +0800

    Initial commit

---
+++ LoadStockInfo/src/main/java/com/alienforg/LoadStockInfo/App.java
@@ -0,0 +1,13 @@
+package com.alienforg.LoadStockInfo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
